9d5ea7452dfaf4f4699809f54813954cf22bd8a9
Dávid El-Saig <david.el-saig@lombiq.com>
2022-05-16 19:49:50 +0200

    Add TestSolutions.

---
+++ TestSolutions/AnalyzerViolations.cs
@@ -0,0 +1,11 @@
+﻿namespace Lombiq.Analyzers.ProjectReference;
+
+// This file has intentional analyzer violations.
+public class Class1
+{
+    private int Number;
+    public Class1(int number)
+    {
+        Number = number;
+    }
+}
