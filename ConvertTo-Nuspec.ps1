0c0fa7bb00fb29ddb3af539db853291b443725ff
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-02 14:28:55 +0200

    Create a nuspec file from tempate.

---
+++ ConvertTo-Nuspec.ps1
@@ -0,0 +1,19 @@
+﻿param($Version)
+
+$templateFileName = [System.IO.Path]::Combine($PWD, 'Lombiq.Analyzers', 'Lombiq.Analyzers.nuspec.template')
+$nuspec = [xml](Get-Content $templateFileName)
+$group = $nuspec.package.metadata.dependencies.group
+
+foreach($dependency in ([xml](Get-Content "CommonPackages.props")).Project.ItemGroup.AnalyzerPackage)
+{
+    $id = $dependency.Include
+    if (-not $id) { continue }
+
+    $node = $nuspec.CreateElement('dependency')
+    $node.SetAttribute('id', $id)
+    $node.SetAttribute('version', $dependency.Version)
+
+    $group.AppendChild($node)
+}
+
+$nuspec.Save([System.IO.Path]::Combine($PWD, 'Lombiq.Analyzers.nuspec'))
