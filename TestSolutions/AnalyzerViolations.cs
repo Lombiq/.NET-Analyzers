bab5c129558c3fecc85cd19bc315ddc97f388f81
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-02 22:51:59 +0200

    Add indirect package reference test.

--- TestSolutions/AnalyzerViolations.cs
+++ TestSolutions/AnalyzerViolations.cs
@@ -1,6 +1,8 @@
 ﻿namespace Lombiq.Analyzers.ProjectReference;
 
-// This file has intentional analyzer violations.
+// This file has intentional analyzer violations. Only edit the one in the TestSolutions directory. Any copy in the
+// subdirectories will be copied over during build. Copying is necessary so duplicate warnings from different projects
+// can be shown.
 public class Class1
 {
     private int Number;
