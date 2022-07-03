0f7e26e4fe65f54aa27641050d89e24ae2d95a28
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-04 01:15:25 +0200

    Copy dlls instead of move.

--- Invoke-BeforePack.ps1
+++ Invoke-BeforePack.ps1
@@ -4,16 +4,16 @@
 # The cause is probably that we don't actually do anything in the corresponding csproj, it only exists because we need
 # an entry point for `dotnet pack` (unlike the older `nuget pack` tooling, which worked with just the .nuspec alone).
 
-Get-ChildItem Lombiq.Analyzers/obj -Recurse -Include Lombiq.Analyzers.dll | Remove-Item
+Remove-Item Lombiq.Analyzers/obj -Recurse
 
 $release = Get-ChildItem Lombiq.Analyzers/bin/Release -Recurse -Include Lombiq.Analyzers.dll
 $releasePdb = $release.FullName -replace 'dll$','pdb'
 New-Item -Type Directory -Force lib
-Move-Item $release lib
+Copy-Item $release lib
 
 if (Test-Path $releasePdb)
 {
-    Move-Item $releasePdb lib
+    Copy-Item $releasePdb lib
 }
 else
 {
