edfcecb81f2c17759cec4854a1c3c234cab7b77e
Dávid El-Saig <david.el-saig@lombiq.com>
2022-07-04 01:29:56 +0200

    Don't delete obj directory.

--- Invoke-BeforePack.ps1
+++ Invoke-BeforePack.ps1
@@ -1,10 +1,8 @@
-# This script prevents NU5100 warning caused by the presence of these files
+# This script tries to work around NuGet's validation and the NU5100 warning caused by the presence of these files
 # - bin/Release/netstandard2.0/Lombiq.Analyzers.dll
 # - obj/Release/netstandard2.0/Lombiq.Analyzers.dll
 # The cause is probably that we don't actually do anything in the corresponding csproj, it only exists because we need
 # an entry point for `dotnet pack` (unlike the older `nuget pack` tooling, which worked with just the .nuspec alone).
-
-Remove-Item Lombiq.Analyzers/obj -Recurse
 
 $release = Get-ChildItem Lombiq.Analyzers/bin/Release -Recurse -Include Lombiq.Analyzers.dll
 $releasePdb = $release.FullName -replace 'dll$','pdb'
