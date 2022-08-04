40b6ff92bf1949c605c2d844e283be01f3801f1e
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-08-04 14:37:28 +0200

    Remove \obj\Release completely before packing

--- Invoke-BeforePack.ps1
+++ Invoke-BeforePack.ps1
@@ -4,10 +4,6 @@
 # The cause is probably that we don't actually do anything in the corresponding csproj, it only exists because we need
 # an entry point for `dotnet pack` (unlike the older `nuget pack` tooling, which worked with just the .nuspec alone).
 
-# Remove all .dll files from the directory to be packed.
-
-# First, the whole \bin folder.
-Remove-Item .\Lombiq.Analyzers\bin -Recurse
-
-# Then, all .dll files under the \obj folder.
-Get-ChildItem Lombiq.Analyzers\obj -Include *.dll -Recurse | Remove-Item
+# Remove all .dll and other build output files from the directory to be packed.
+Remove-Item Lombiq.Analyzers/bin -Recurse
+Remove-Item Lombiq.Analyzers/obj/Release -Recurse
