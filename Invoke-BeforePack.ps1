8fd36019aa478b76c54f48dca5cb0084ad56fd13
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-08-04 11:17:57 +0200

    Trying to fix NU5104

--- Invoke-BeforePack.ps1
+++ Invoke-BeforePack.ps1
@@ -4,16 +4,6 @@
 # The cause is probably that we don't actually do anything in the corresponding csproj, it only exists because we need
 # an entry point for `dotnet pack` (unlike the older `nuget pack` tooling, which worked with just the .nuspec alone).
 
-$release = Get-ChildItem Lombiq.Analyzers/bin/Release -Recurse -Include Lombiq.Analyzers.dll
-$releasePdb = $release.FullName -replace 'dll$','pdb'
 New-Item -Type Directory -Force lib
-Copy-Item $release lib
-
-if (Test-Path $releasePdb)
-{
-    Copy-Item $releasePdb lib
-}
-else
-{
-    Write-Warning "There is no $releasePdb file!"
-}
+Get-ChildItem bin\Debug | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.dll
+Get-ChildItem bin\Debug | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.pdb
