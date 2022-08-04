33ee340cf63f10835b721b02a07089c468b3c186
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-08-04 12:08:34 +0200

    Explicit paths as proof-of-concept

--- Invoke-BeforePack.ps1
+++ Invoke-BeforePack.ps1
@@ -4,11 +4,7 @@
 # The cause is probably that we don't actually do anything in the corresponding csproj, it only exists because we need
 # an entry point for `dotnet pack` (unlike the older `nuget pack` tooling, which worked with just the .nuspec alone).
 
-
-#New-Item -Type Directory -Force lib
-#Get-ChildItem Lombiq.Analyzers/bin/Release | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.dll
-#Get-ChildItem Lombiq.Analyzers/bin/Release | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.pdb
-
-Get-ChildItem Lombiq.Analyzers/lib
-
-Get-ChildItem Lombiq.Analyzers/lib/netstandard2.0
+$framework = "netstandard2.0"
+New-Item -Type Directory -Force Lombiq.Analyzers/lib/$framework
+Copy-Item Lombiq.Analyzers/bin/Release/$framework/Lombiq.Analyzers.dll -Destination Lombiq.Analyzers/lib/$framework
+Copy-Item Lombiq.Analyzers/bin/Release/$framework/Lombiq.Analyzers.dll -Destination Lombiq.Analyzers/lib/$framework
