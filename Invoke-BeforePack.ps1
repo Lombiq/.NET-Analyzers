364bac666c32ce03503819c68e0c7edfad758331
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-08-04 12:33:52 +0200

    Restore working version

--- Invoke-BeforePack.ps1
+++ Invoke-BeforePack.ps1
@@ -4,7 +4,16 @@
 # The cause is probably that we don't actually do anything in the corresponding csproj, it only exists because we need
 # an entry point for `dotnet pack` (unlike the older `nuget pack` tooling, which worked with just the .nuspec alone).
 
-$framework = "netstandard2.0"
-New-Item -Type Directory -Force Lombiq.Analyzers/lib/$framework
-Copy-Item Lombiq.Analyzers/bin/Release/$framework/Lombiq.Analyzers.dll -Destination Lombiq.Analyzers/lib/$framework
-Copy-Item Lombiq.Analyzers/bin/Release/$framework/Lombiq.Analyzers.dll -Destination Lombiq.Analyzers/lib/$framework
+$release = Get-ChildItem Lombiq.Analyzers/bin/Release -Recurse -Include Lombiq.Analyzers.dll
+$releasePdb = $release.FullName -replace 'dll$','pdb'
+New-Item -Type Directory -Force lib
+Copy-Item $release lib
+
+if (Test-Path $releasePdb)
+{
+    Copy-Item $releasePdb lib
+}
+else
+{
+    Write-Warning "There is no $releasePdb file!"
+}
