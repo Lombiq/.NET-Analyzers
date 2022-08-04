76b8a5f33ca810bd3735af570bfff9b30ef5553b
Oliver Friedrich <oliver.friedrich@lombiq.com>
2022-08-04 11:46:24 +0200

    Debugging the lib folder

--- Invoke-BeforePack.ps1
+++ Invoke-BeforePack.ps1
@@ -4,6 +4,11 @@
 # The cause is probably that we don't actually do anything in the corresponding csproj, it only exists because we need
 # an entry point for `dotnet pack` (unlike the older `nuget pack` tooling, which worked with just the .nuspec alone).
 
-New-Item -Type Directory -Force lib
-Get-ChildItem Lombiq.Analyzers/bin/Release | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.dll
-Get-ChildItem Lombiq.Analyzers/bin/Release | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.pdb
+
+#New-Item -Type Directory -Force lib
+#Get-ChildItem Lombiq.Analyzers/bin/Release | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.dll
+#Get-ChildItem Lombiq.Analyzers/bin/Release | Copy-Item -Destination lib -Recurse -filter Lombiq.Analyzers.pdb
+
+Get-ChildItem Lombiq.Analyzers/lib
+
+Get-ChildItem Lombiq.Analyzers/lib/netstandard2.0
